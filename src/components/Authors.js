5fc55e30564adfa7a6ea799ad81289c2416cc86d
Izhan <babuizhan1234@gmail.com>
2021-09-15 21:50:23 +0530

    update authors

--- src/components/Authors.js
+++ src/components/Authors.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { gql, useQuery } from "@apollo/client";
+import React, { useState } from "react";
+import { gql, useQuery, useMutation } from "@apollo/client";
 
 const ALL_AUTHORS = gql`
   query {
@@ -10,8 +10,24 @@ const ALL_AUTHORS = gql`
     }
   }
 `;
+
+const ADD_AUTHORS = gql`
+  mutation Mutation($author: String!, $date: Int!) {
+    editAuthor(name: $author, setBornTo: $date) {
+      name
+      born
+    }
+  }
+`;
+
 const Authors = (props) => {
+  const [author, setAuthor] = useState("Robert Martin");
+  const [date, setDate] = useState(0);
   const authors = useQuery(ALL_AUTHORS);
+
+  const [updatedauthor] = useMutation(ADD_AUTHORS, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
 
   if (!props.show) {
     return null;
@@ -21,6 +37,24 @@ const Authors = (props) => {
     return <div>loading...</div>;
   }
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event);
+    console.log(author);
+    console.log(date);
+    updatedauthor({ variables: { author, date } });
+  };
+
+  const handleAuthor = (event) => {
+    event.preventDefault();
+    console.log(event);
+    setAuthor(event.target.value);
+  };
+  const handleDate = (event) => {
+    event.preventDefault();
+    console.log(event);
+    setDate(event.target.value);
+  };
   return (
     <div>
       <h2>authors</h2>
@@ -40,6 +74,19 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={handleSubmit}>
+        <select value={author} onChange={handleAuthor}>
+          {authors.data.allAuthors.map((a) => (
+            <option value={a.name} key={a.name}>
+              {a.name}
+            </option>
+          ))}
+        </select>
+        <label htmlFor="born">born</label>
+        <input type="number" id="born" onChange={handleDate} />
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
